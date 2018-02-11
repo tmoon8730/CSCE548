55a6beeb4efe31a3707bfd414518ff561293350a
Seed <seed@ubuntu.(none)>
2018-02-11 10:35:35 -0800

    Started integrating the words.txt

--- project1/project1.c
+++ project1/project1.c
@@ -3,32 +3,54 @@
 #include <openssl/err.h>
 #include <openssl/aes.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
 	FILE *ifp = fopen(argv[1], "r");
 	FILE *ofp = fopen(argv[2], "w");
 
-	int bytes_read, bytes_written;
-	unsigned char indata[AES_BLOCK_SIZE];
-	unsigned char outdata[AES_BLOCK_SIZE];
+	int i = 0;
+	char dict[25144];
+	char line[25144];
 
-	unsigned char ckey[] = "thiskeyisveybad";
-	unsigned char iv[] = "dontusethisinput";
 
-	AES_KEY key;
-
-	AES_set_encrypt_key(ckey, 128, &key);
-
-	int num = 0;
-
-	while(1) {
-		bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
-		AES_cfb128_encrypt(indata, outdata, bytes_read, &key, iv, &num, AES_DECRYPT);
-    bytes_written = fwrite(outdata, 1, bytes_read, ofp);
-		if(bytes_read < AES_BLOCK_SIZE)
-			break;
+	FILE *dictionary = fopen(argv[3], "r");
+	while(fgets(line, sizeof line, dictionary) != NULL) {
+		dict[i] = line;
+		i++;
 	}
+	int k = 0;
+	for(k = 0; k < 25144; i++){
+		int bytes_read, bytes_written;
+		unsigned char indata[AES_BLOCK_SIZE];
+		unsigned char outdata[AES_BLOCK_SIZE];
+
+		unsigned char ckey[] = dict[k];
+		unsigned char iv[] = "dontusethisinput";
+
+		AES_KEY key;
+
+		AES_set_encrypt_key(ckey, 128, &key);
+
+		int num = 0;
+
+		while(1) {
+			bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
+			AES_cfb128_encrypt(indata, outdata, bytes_read, &key, iv, &num, AES_DECRYPT);
+		  bytes_written = fwrite(outdata, 1, bytes_read, ofp);
+			if(bytes_read < AES_BLOCK_SIZE)
+				break;
+		}
+	}
+	
+	
+
+
+	fclose(dictionary);
+	fclose(ifp);
+	fclose(ofp);
 	return 1;
 }
 
