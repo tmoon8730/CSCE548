fc9da78a89e3f334e2c67003a4717768ce0f6069
Seed <seed@ubuntu.(none)>
2018-02-12 10:56:16 -0800

    Got it bois

--- project1/project1.c
+++ project1/project1.c
@@ -6,138 +6,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void pad(char *s, int length);
+int print_result(unsigned char *buf, char *s, int len, FILE *outFile, char *match);
+int strcicmp(char const *a, char const *b);
+
 int main(int argc, char** argv)
 {
-	
-	char PLAIN_TEXT[] = "This is a top secret.";
-	
-	if(argc < 4){
-		printf("Not enough args\n");
-		return 1;
-	}
-	FILE *ifp = fopen(argv[1], "r");
-	FILE *ofp = fopen(argv[2], "w");
-	
-	char const* const fileName = argv[3];
-	FILE* file = fopen(fileName, "r");
-	char line[256];
+	unsigned char match[] = "Match";
+	unsigned char noMatch[] = "Not a match";
 
-	while(fgets(line, sizeof(line), file)){
-		printf("%s", line);
-		int bytes_read, bytes_written;
-		unsigned char indata[AES_BLOCK_SIZE];
-		unsigned char outdata[AES_BLOCK_SIZE];
+	int i;
+	char words[16], t; // Each word in the dictionary
+	FILE *key, *outFile;
+	unsigned char outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
+	unsigned char iv[16] = {0}; // Given
+	int outlen, tmplen;
+	int num;
 
-		char ckey[256];
-		strcpy(ckey, line);		
-		// = dict[k];
-		char iv[] = "dontusethisinput";
-
-		AES_KEY key;
-
-		AES_set_encrypt_key(ckey, 128, &key);
-
-		int num = 0;
-
-		for(;;) {
-			bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
-			AES_cfb128_encrypt(indata, outdata, bytes_read, &key, iv, &num, AES_DECRYPT);
-			printf("%s",outdata);
-			if(outdata == PLAIN_TEXT){
-				printf("FOUND IT! %s", line);
-				return 0;
-			}
-		  bytes_written = fwrite(outdata, 1, bytes_read, ofp);
-			if(bytes_read < AES_BLOCK_SIZE)
-				break;
-		}
-	}
-	
-	/*int i = 0;
-	char *dict[25144];
-	char line[25144];
-
-
-	FILE *dictionary = fopen(argv[3], "r");
-	while(fgets(line, sizeof line, dictionary) != NULL) {
-		dict[i] = line;
-		i++;
-	}
-	int k = 0;
-	for(k = 0; k < sizeof(dict); k++){
-		//printf("Checking %i", k);
-		int bytes_read, bytes_written;
-		unsigned char indata[AES_BLOCK_SIZE];
-		unsigned char outdata[AES_BLOCK_SIZE];
-
-		char ckey[16];
-		strcpy(ckey, dict[k]);		
-		// = dict[k];
-		char iv[] = "dontusethisinput";
-
-		AES_KEY key;
-
-		AES_set_encrypt_key(ckey, 128, &key);
-
-		int num = 0;
-
-		while(1) {
-			bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
-			AES_cfb128_encrypt(indata, outdata, bytes_read, &key, iv, &num, AES_DECRYPT);
-		  bytes_written = fwrite(outdata, 1, bytes_read, ofp);
-			if(bytes_read < AES_BLOCK_SIZE)
-				break;
-		}
-	}*/
-	
-	
-
-
-	//fclose(dictionary);
-	fclose(ifp);
-	fclose(ofp);
-	fclose(file);
-	return 1;
-}
-
-
-
-
-	/*FILE *in = fopen(argv[1], "r");
-	FILE *out = fopen(argv[2], "w");
-	int do_encrypt = (int)argv[3];
-	printf("Got the files");
-
-  unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
-	int inlen, outlen;
-  EVP_CIPHER_CTX ctx;
- 	unsigned char key[] = "0123456789abcdeF";
-	unsigned char iv[] = "1234567887654321";
-	
+	EVP_CIPHER_CTX ctx;
 	EVP_CIPHER_CTX_init(&ctx);
-	EVP_CipherInit_ex(&ctx, EVP_aes_128_cbc(), NULL, NULL, NULL, do_encrypt);
-	OPENSSL_assert(EVP_CIPHER_CTX_key_length(&ctx) == 16);
-	OPENSSL_assert(EVP_CIPHER_CTX_iv_length(&ctx) == 16);
+	char inText[] = "This is a top secret."; // Given
+	char cipherTextGiven[] = "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9"; // given
+	key = fopen("words.txt", "r"); // given
+	if(remove("matchingResult.txt") == -1){
+		perror("Error deleting file");
+	}
+	outFile = fopen("matchingResult.txt", "a+"); // Open the matching file with append mode
+	if( key < 0 || outFile < 0){
+		perror("Connot open file");
+		exit(1);
+	}
 
-	EVP_CipherInit_ex(&ctx, NULL, NULL, key, iv, do_encrypt);
 
-	for(;;){
-		inlen = fread(inbuf, 1, 1024, in);
-		if(inlen <= 0) break;
-		if(!EVP_CipherUpdate(&ctx, outbuf, &outlen, inbuf, inlen)){
+	char pbuffer[1024];
+
+	while(fgets(words, 16, key)) {
+		i = strlen(words); // number of words
+		words[i-1]='\0'; // Remove null or end of file characters
+		i = strlen(words); // Set the value again
+		if(i < 16){ // Use 16 because of AES-128
+			pad(words, (16)); // Need to add space chars to the end of words that are shorter
+					 						  // than 16 characters long
+		}
+		EVP_EncryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, words, iv); // Set the encryption method 
+									     																					  // to aes 128 cbc
+		if(!EVP_EncryptUpdate(&ctx, outbuf, &outlen, inText, strlen(inText))){
 			EVP_CIPHER_CTX_cleanup(&ctx);
 			return 0;
 		}
-		fwrite(outbuf, 1, outlen, out);
-		printf("Writing output");
-	}
-	if(!EVP_CipherFinal_ex(&ctx, outbuf, &outlen))
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return 0;
-	}
-	fwrite(outbuf, 1, outlen, out);
+		if(!EVP_EncryptFinal_ex(&ctx, outbuf + outlen, &tmplen)){
+			EVP_CIPHER_CTX_cleanup(&ctx);
+			return 0;
+		}
+		outlen += tmplen;
 
-	EVP_CIPHER_CTX_cleanup(&ctx);
-  return 1;
-}*/
+		int i;
+		char* buf_str = (char*) malloc (2*outlen + 1);
+		char* buf_ptr = buf_str;
+		for(i=0;i<outlen;i++){
+			buf_ptr += sprintf(buf_ptr, "%02X", outbuf[i]);
+		}
+		*(buf_ptr + 1) = '\0';
+		if(strcicmp(cipherTextGiven, buf_str) == 0){
+			print_result(outbuf, words, outlen, outFile, match);
+		}else{
+			print_result(outbuf, words, outlen, outFile, noMatch);
+		}
+	}
+	fclose(key);
+	fclose(outFile);
+	return 0;
+}
+
+// Print results to file matchResult.txt
+int print_result(unsigned char *buf, char *s, int len, FILE *outFile, char *match){
+	int i,n,j,k;
+	char x='\n';
+	char space=' ';
+	for(j=0;j<strlen(s);j++){
+		fprintf(outFile,"%c",s[j]);
+	}
+	fprintf(outFile,"%c",space);
+	for(i=0;i<len;i++){
+		fprintf(outFile,"%02x",buf[i]);
+	}
+	fprintf(outFile,"%c",space);
+	for(k=0;k<strlen(match);k++){
+		fprintf(outFile,"%c",match[k]);
+	}
+	fprintf(outFile,"%c",x);
+	return (0);
+}
+
+// Add space padding to words shorter than 16
+void pad(char *s, int length){
+	int l;
+	l = strlen(s);
+	while(l<length){
+		s[l] = ' '; // Insert a space
+		l++;
+	}
+	s[l] = '\0'; // terminate the string with a null
+}
+
+// Compare case insensitive
+int strcicmp(char const *a, char const *b){
+	for(;; a++, b++){
+		int d = tolower(*a) - tolower(*b);
+		if(d != 0 || !*a)
+			return d;
+	}
+}
