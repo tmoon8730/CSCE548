c670983b329042427c60b205d6051acfd612144b
Seed <seed@ubuntu.(none)>
2018-02-12 17:03:45 -0800

    Refactoring

--- project1/project1.c
+++ project1/project1.c
@@ -14,24 +14,24 @@ int main(int argc, char** argv)
 {
 	unsigned char match[] = "Match";
 	unsigned char noMatch[] = "Not a match";
+	unsigned char iv[16] = {0}; // Given
 
-	int i;
 	char words[16], t; // Each word in the dictionary
 	FILE *key, *outFile;
 	unsigned char outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
-	unsigned char iv[16] = {0}; // Given
+
 	int outlen, tmplen;
 	int num;
 
 	EVP_CIPHER_CTX ctx;
 	EVP_CIPHER_CTX_init(&ctx);
 	char inText[] = "This is a top secret."; // Given
-	char cipherTextGiven[] = "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9"; // given
-	key = fopen("words.txt", "r"); // given
-	if(remove("matchingResult.txt") == -1){
+	char cipherText[] = "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9"; // given
+	key = fopen("words.txt", "r"); // Given
+	if(remove("output.txt") == -1){
 		perror("Error deleting file");
 	}
-	outFile = fopen("matchingResult.txt", "a+"); // Open the matching file with append mode
+	outFile = fopen("output.txt", "a+"); // Open the matching file with append mode
 	if( key < 0 || outFile < 0){
 		perror("Connot open file");
 		exit(1);
@@ -39,14 +39,22 @@ int main(int argc, char** argv)
 
 
 	char pbuffer[1024];
-
+	int i;
 	while(fgets(words, 16, key)) {
+
 		i = strlen(words); // number of words
 		words[i-1]='\0'; // Remove null or end of file characters
 		i = strlen(words); // Set the value again
 		if(i < 16){ // Use 16 because of AES-128
-			pad(words, (16)); // Need to add space chars to the end of words that are shorter
+			//pad(words, (16)); // Need to add space chars to the end of words that are shorter
 					 						  // than 16 characters long
+			int l;
+			l = strlen(words);
+			while(l<16){
+				words[l] = ' '; // Insert a space
+				l++;
+			}
+			words[l] = '\0'; // terminate the string with a null		
 		}
 		EVP_EncryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, words, iv); // Set the encryption method 
 									     																					  // to aes 128 cbc
@@ -67,10 +75,24 @@ int main(int argc, char** argv)
 			buf_ptr += sprintf(buf_ptr, "%02X", outbuf[i]);
 		}
 		*(buf_ptr + 1) = '\0';
-		if(strcicmp(cipherTextGiven, buf_str) == 0){
-			print_result(outbuf, words, outlen, outFile, match);
+
+		// Compare against the known decrypted value
+		int stringCheck;
+		char const *a, *b;
+		a = cipherText;
+		b = buf_str;
+		for(;; a++, b++){
+			int d = tolower(*a) - tolower(*b);
+			if(d != 0 || !*a){
+				stringCheck = d;
+				break;
+			}
+		}
+		// If the value is 0 then it matches and the correct key was found
+		if(stringCheck == 0){
+			outputresult(outbuf, words, outlen, outFile, match);
 		}else{
-			print_result(outbuf, words, outlen, outFile, noMatch);
+			outputresult(outbuf, words, outlen, outFile, noMatch);
 		}
 	}
 	fclose(key);
@@ -78,8 +100,8 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-// Print results to file matchResult.txt
-int print_result(unsigned char *buf, char *s, int len, FILE *outFile, char *match){
+// Print results to file output.txt
+int outputresult(unsigned char *buf, char *s, int len, FILE *outFile, char *match){
 	int i,n,j,k;
 	char x='\n';
 	char space=' ';
@@ -96,24 +118,4 @@ int print_result(unsigned char *buf, char *s, int len, FILE *outFile, char *matc
 	}
 	fprintf(outFile,"%c",x);
 	return (0);
-}
-
-// Add space padding to words shorter than 16
-void pad(char *s, int length){
-	int l;
-	l = strlen(s);
-	while(l<length){
-		s[l] = ' '; // Insert a space
-		l++;
-	}
-	s[l] = '\0'; // terminate the string with a null
-}
-
-// Compare case insensitive
-int strcicmp(char const *a, char const *b){
-	for(;; a++, b++){
-		int d = tolower(*a) - tolower(*b);
-		if(d != 0 || !*a)
-			return d;
-	}
 }
